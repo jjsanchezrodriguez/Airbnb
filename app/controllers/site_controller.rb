4aa6e482949674a0e57cc66570e6b2980c6715e0
Jorge <jjsanchezrodriguez@gmail.com>
2015-12-09 23:39:37 +0100

    new flat

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -3,4 +3,24 @@ class SiteController < ApplicationController
 			@flats = Flat.all
 	end
 
+	def new
+			@flats = Flat.new
+	end
+
+	def create
+			flat = Flat.new(
+				:title => params[:flat][:title],
+				:city => params[:flat][:city],
+				:owner => params[:flat][:owner],
+				:description => params[:flat][:description],
+				:price => params[:flat][:price],
+				:pic_url => params[:flat][:pic_url])
+
+			flat.save
+
+			redirect_to("/site")
+
+
+	end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  get '/' => 'site#home', as: :home
+  get '/site' => 'site#home', as: :home
   get '/become' => 'site#become', as: :become
   get '/sign' => 'site#sign', as: :sign
   get '/log' => 'site#log', as: :log
   get '/help' => 'site#help', as: :help
+  get '/new'  => 'site#new'
+  post '/site'  => 'site#create'
 end
